0d1282fe8e4afb482de39d5fc13b1cb4f2a9824f
Danial2130 <danialarshad2103@gmail.com>
2026-09-05 23:07:48 +0700

    inisial commit

---
+++ hello.py
@@ -0,0 +1,35 @@
+print("hello world")
+# ini komentar
+print("Halo Danial")
+
+nama = "danial"
+umur = 18
+
+nama_depan = "danial"
+
+print(type(umur))
+print(type(nama))
+
+puisi="""
+diamond diamond
+la peace
+la peace
+"""
+
+print(type(puisi))
+
+is_student = True
+
+print(type(is_student))
+
+skor = 30
+print(skor)
+
+skor = 90
+print(skor)
+
+panjang = 15
+lebar = 5
+luas = panjang * lebar
+
+print("Luas: ", luas)
