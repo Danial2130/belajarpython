49896ddb98b2290e29906ddde0b55faff80d6462
Danial2130 <danialarshad2103@gmail.com>
2026-09-06 22:13:43 +0700

    tipe data dan variable

---
+++ inout.py
@@ -0,0 +1,10 @@
+nama = input("Masukan nama anda: ")
+print("Hello", nama)
+
+umur_text = input("Masukan umur anda: ")
+umur = int(umur_text)#konversi output yang seharuny str ke int
+
+print("Umur anda ", umur_text)
+print("Tipe umur",type(umur))
+#output dari input selalu string
+
